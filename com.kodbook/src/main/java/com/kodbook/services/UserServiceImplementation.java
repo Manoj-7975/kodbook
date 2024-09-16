20b9e881c72f29efdd54468b0e0a6f8bd71f9cd0
Manoj7975 <manojmanu32820@gmail.com>
2024-09-16 15:04:46 +0530

    2nd commit

--- com.kodbook/src/main/java/com/kodbook/services/UserServiceImplementation.java
+++ com.kodbook/src/main/java/com/kodbook/services/UserServiceImplementation.java
@@ -7,33 +7,32 @@ import com.kodbook.entities.User;
 import com.kodbook.repositories.UserRepository;
 
 @Service
-public class UserServiceImplementation  implements UserService{
+public class UserServiceImplementation
+		implements UserService{
 	@Autowired
 	UserRepository repo;
-	
+
 	public void addUser(User user) {
 		repo.save(user);
-		
 	}
 
-@Override
+	@Override
 	public boolean userExists(String username, String email) {
-		User user1=repo.findByUsername(username);
-		User user2=repo.findByEmail(email);
-		if(user1!=null||user2!=null) {
+		User user1 = repo.findByUsername(username);
+		User user2 = repo.findByEmail(email);
+		if(user1 != null || user2 !=null) {
 			return true;
 		}
 		return false;
 	}
 
-@Override
-public boolean validateUser(String username, String password) {
-	String dbPass=repo.findByUsername(username).getPassword();
-	if(password.equals(dbPass)) {
-		return true;
+	@Override
+	public boolean validateUser(String username, String password) {
+		String dbPass = repo.findByUsername(username).getPassword();
+		if(password.equals(dbPass)) {
+			return true;
+		}
+		return false;
 	}
-	return false;
-}
-
-
+	
 }
